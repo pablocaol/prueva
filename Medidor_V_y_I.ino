a38ca392c74a43d9b6d747852349a7aee06a0ca2
Pablo Camacho <32745440+pablocaol@users.noreply.github.com>
2020-01-11 20:02:41 -0600

    Add files via upload

---
+++ Medidor_V_y_I.ino
@@ -0,0 +1,35 @@
+#include "EmonLib.h"
+
+
+EnergyMonitor emon;
+#define vCalibration 106.8
+#define currCalibration 0.52
+
+
+float kWh = 0;
+
+unsigned long lastmillis = millis();
+
+void setup() {
+  Serial.begin(9600);
+  emon.voltage(38, vCalibration, 1.7); // Voltage: input pin, calibration, phase_shift
+  emon.current(39, currCalibration); // Current: input pin, calibration.
+}
+
+void loop() {
+    emon.calcVI(20, 2000);
+    Serial.print("Vrms: ");
+    Serial.print(emon.Vrms, 2);
+    Serial.print("V");
+    Serial.print("\tIrms: ");
+    Serial.print(emon.Irms, 4);
+    Serial.print("A");
+    Serial.print("\tPower: ");
+    Serial.print(emon.apparentPower, 4);
+    Serial.print("W");
+    Serial.print("\tkWh: ");
+    kWh = kWh + emon.apparentPower*(millis()-lastmillis)/3600000000.0;
+    Serial.print(kWh, 4);
+    Serial.println("kWh");
+    lastmillis = millis();
+}
